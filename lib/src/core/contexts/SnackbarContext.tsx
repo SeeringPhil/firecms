da2dc10d04c642344f50089ebdf3408acee57bcf
Philippe Boulanger <pboulanger@autoroot.io>
2023-03-24 00:30:33 -0400

    Add props to the SnackbarProvider so users can change maxSnack and autoHideDuration

--- lib/src/core/contexts/SnackbarContext.tsx
+++ lib/src/core/contexts/SnackbarContext.tsx
@@ -1,11 +1,11 @@
 import React, { PropsWithChildren } from "react";
 import { SnackbarProvider as NotistackSnackbarProvider } from "notistack";
 
-export const SnackbarProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+export const SnackbarProvider: React.FC<PropsWithChildren<{maxSnack: number, autoHideDuration: number }>> = ({ children, maxSnack = 3, autoHideDuration = 3500 }) => {
 
     return (
-        <NotistackSnackbarProvider maxSnack={3}
-                                   autoHideDuration={3500}>
+        <NotistackSnackbarProvider maxSnack={maxSnack}
+                                   autoHideDuration={autoHideDuration}>
             {children}
         </NotistackSnackbarProvider>
     );

9f239256505a181c2ebd5ca1198f00ce97866ace
Philippe Boulanger <pboulanger@autoroot.io>
2023-03-24 00:53:09 -0400

    Make sure the props are optional

--- lib/src/core/contexts/SnackbarContext.tsx
+++ lib/src/core/contexts/SnackbarContext.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 import { SnackbarProvider as NotistackSnackbarProvider } from "notistack";
 
-export const SnackbarProvider: React.FC<PropsWithChildren<{maxSnack: number, autoHideDuration: number }>> = ({ children, maxSnack = 3, autoHideDuration = 3500 }) => {
+export const SnackbarProvider: React.FC<PropsWithChildren<{maxSnack?: number, autoHideDuration?: number }>> = ({ children, maxSnack = 3, autoHideDuration = 3500 }) => {
 
     return (
         <NotistackSnackbarProvider maxSnack={maxSnack}

3e356e48f44a7cf628909812f51145962939f837
Philippe <pboulanger@autoroot.io>
2023-05-16 00:17:45 -0400

    Make the example open on localhost instead of 127.0.0.1 (this is so firebase lets us open projects correctly instead of having to add 127.0.0.1 manually) Start on another (unusual) port, Set vite to open the page by default

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,10 +3,18 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import ViteFonts from "vite-plugin-fonts"
 
+import dns from "dns"
+
+dns.setDefaultResultOrder("verbatim")
+
 // https://vitejs.dev/config/
 export default defineConfig({
     esbuild: {
         logOverride: { "this-is-undefined-in-esm": "silent" }
+    },
+    server: {
+        port: 9999,
+        open: true
     },
     build: {
         outDir: "./build",
